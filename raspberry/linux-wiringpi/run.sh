9a53dd65e465addbadd37287f4fac06e3da8930e
Bertrand Martel <bmartel.fr@gmail.com>
2020-03-08 02:22:19 +0100

    add raspberry-dev + auto download liblouis during build

--- raspberry/linux-wiringpi/run.sh
+++ raspberry/linux-wiringpi/run.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+LIBLOUIS_DIR=liblouis
+
+if [ ! -d ${LIBLOUIS_DIR} ]; then
+	git clone git://github.com/liblouis/liblouis.git
+	cd liblouis
+	git submodule update --init --recursive
+	cd ..
+fi
+
 CUR_DIR=$PWD
 
 ./raspberry-dev/init.sh
@@ -9,7 +18,7 @@ CUR_DIR=$PWD
 # cross compile liblouis
 export PATH=$PATH:$PWD/raspberry-dev/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin
 
-cd ../liblouis
+cd ./liblouis
 if [ ! -f configure ]; then
 	./autogen.sh
 fi
@@ -30,6 +39,6 @@ cd $CUR_DIR
 cmake . -DCMAKE_TOOLCHAIN_FILE=./raspberry-dev/toolchain.cmake
 make
 mkdir -p out_liblouis
-cp ../liblouis/liblouis/.libs/liblouis.* out_liblouis/
-cp -r ../liblouis/tables out_liblouis/
+cp ./liblouis/liblouis/.libs/liblouis.* out_liblouis/
+cp -r ./liblouis/tables out_liblouis/
 cp liblouis_install.sh out_liblouis/
